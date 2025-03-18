7e20bc4cbb0f6e26f34a90d12337b0f73d3b9305
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-18 11:24:22 +0300

    Updated index.html

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let charge 
+  if (feet <=400) {
+    charge = 0;
+    return('This one is on me!');
+  }
+  else if (feet <2000 && feet > 400) {
+    return(`That will be twenty bucks.`);
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    return('I will gladly take your thirty bucks.');
+  }
+  else {
+    return("No can do.");
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? ('Ok, sounds good.')
+  : ("No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous"){
+    return("Thank you so much.");
+  }
+  else if (tip === "not as generous"){
+    return("Thank you.");
+  }
+  else {
+    return("Bye.")
+  }
 }
